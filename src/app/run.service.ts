2545e203b6d5a830d1d24a78aa49027d75290109
Jules <vandrunen.julia@gmail.com>
2018-10-08 08:53:50 +0200

    http requests from backend kinda working now

--- src/app/run.service.ts
+++ src/app/run.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Run } from './run';
 import { RUNS} from './mock-runs';
 import { Training } from './training';
@@ -10,13 +13,46 @@ import { TRAININGS} from './mock-trainings';
 })
 export class RunService {
 
+  private runSortedUrl = "http://localhost:8080/api/run/sortbydate"
+  private allTrainingsUrl = "http://localhost:8080/api/training/all"
+  private baseRunUrl = "http://localhost:8080/api/run"
+  private updateRunUrl = "https://localhost:8080/api/run/update"
+  
+    
+  
+
   getRuns(): Observable<Run[]> {
-    return of (RUNS);
+    return this.http.get<Run[]>(this.runSortedUrl).pipe(
+      catchError(this.handleError('getRuns', []))
+    );
+  }
+
+  getRunById(id:number): Observable<Run> {
+    const url='{this.baseRunUrl}/${id}';
+    return this.http.get<Run>(url).pipe(
+      catchError(this.handleError<Run>('getRunById=${id}'))
+    );
+  }
+
+  UpdateRun(run: Run): Observable<any> {
+    const url = '{this.updateRunUrl}/${hero.id}'
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    return this.http.put(url, run, httpOptions).pipe(catchError(this.handleError<any>('updateHero')));
   }
 
   get Trainings(): Observable<Training[]> {
     return of (TRAININGS);
   }
-
-  constructor() { }
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+   
+      console.error(error); 
+   
+      return of(result as T);
+    };
+  }
+  
+  constructor(private http: HttpClient) { }
 }
