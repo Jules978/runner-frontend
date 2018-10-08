92de7819122e56df7b707a16be40358a4b096910
Jules <vandrunen.julia@gmail.com>
2018-10-08 11:44:49 +0200

    added components to routing, added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,16 @@ import { CommonModule } from '@angular/common';
 
 import { RunsComponent } from './runs/runs.component';
 import { TrainingsComponent} from './trainings/trainings.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
+import { CreateTrainingComponent } from './create-training/create-training.component';
+import { CreateRunComponent } from './create-run/create-run.component';
 
 const routes: Routes = [
   {path: 'runs', component: RunsComponent},
-  {path: 'trainings', component: TrainingsComponent}
+  {path: 'trainings', component: TrainingsComponent},
+  {path: '', component: DashboardComponent},
+  {path: 'addrun', component: CreateRunComponent},
+  {path: 'addtraining', component: CreateTrainingComponent}
 ];
 
 @NgModule({
